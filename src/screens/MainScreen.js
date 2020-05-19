7c771e21d92fc7d19b89a511051de7bf5124af66
ayurkevich <artur.yurkevich.it@gmail.com>
2020-05-18 23:00:21 -0400

    Dimensions: change screen size

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,16 +1,35 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {View, StyleSheet, FlatList, Text, Image} from 'react-native'
 import {AddTodo} from '../components/AddTodo';
 import {Todo} from '../components/Todo';
+import {Dimensions} from "react-native-web";
+import {THEME} from "../theme";
 
 export const MainScreen = ({addTodo, todos, removeTodo, openTodo}) => {
-  let content = <FlatList
-    keyExtractor={item => item.id.toString()}
-    data={todos}
-    renderItem={({item}) => (
-      <Todo todo={item} onRemove={removeTodo} onOpen={openTodo}/>
-    )}
-  />
+  const [deviceWidth, setDeviceWidth] = useState(Dimensions.get('window').width)
+
+  useEffect(() => {
+    const update = () => {
+      const width = useState(Dimensions.get('window').width)
+      setDeviceWidth(width)
+    }
+    Dimensions.addEventListener('change', update)
+    return () => {
+      Dimensions.removeEventListener('change')
+    }
+  })
+
+  let content = (
+    <View style={{width: deviceWidth}}>
+      <FlatList
+        keyExtractor={item => item.id.toString()}
+        data={todos}
+        renderItem={({item}) => (
+          <Todo todo={item} onRemove={removeTodo} onOpen={openTodo}/>
+        )}
+      />
+    </View>
+  )
 
   if (todos.length === 0) {
     content = <View style={styles.imgWrap}>
