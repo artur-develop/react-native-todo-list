c4631b256cfd8754ecd89148b02a547ce6898d5c
ayurkevich <artur.yurkevich.it@gmail.com>
2020-05-18 22:26:04 -0400

    different styles for Android and Ios

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,10 @@ import {THEME} from '../theme'
 // export const Navbar = ({title}) => {
 export const Navbar = (props) => {
   return (
-    <View style={styles.navbar}>
+    <View style={{...styles.navbar, ...Platform.select({
+        ios: styles.navbarIos,
+        android: styles.navbarAndroid
+      })}}>
       <Text style={styles.text}>{props.title}</Text>
     </View>
   )
@@ -19,8 +22,15 @@ const styles = StyleSheet.create({
     backgroundColor: THEME.MAIN_COLOR,
     paddingBottom: 10
   },
+  navbarAndroid: {
+    backgroundColor: THEME.MAIN_COLOR
+  },
+  navbarIos: {
+    borderBottomColor: THEME.MAIN_COLOR,
+    borderBottomWidth: 1
+  },
   text: {
-    color: '#fff',
+    color: Platform.OS === 'ios' ? THEME.MAIN_COLOR : '#fff',
     fontSize: 20
   }
 })
--- src/components/ui/AppButton.js
+++ src/components/ui/AppButton.js
@@ -1,15 +1,18 @@
 import React from 'react'
-import {View, StyleSheet, TouchableOpacity} from 'react-native'
+import {View, StyleSheet, TouchableOpacity, TouchableNativeFeedback, Platform} from 'react-native'
 import {AppTextBold} from './AppTextBold'
 import {THEME} from '../../theme';
 
-export const AppButton = ({children, onPress, color = THEME.MAIN_COLOR}) => (
-  <TouchableOpacity onPress={onPress} activeOpacity={0.8}>
-    <View style={{...styles.button, backgroundColor: color}}>
-      <AppTextBold style={styles.text}>{children}</AppTextBold>
-    </View>
-  </TouchableOpacity>
-)
+export const AppButton = ({children, onPress, color = THEME.MAIN_COLOR}) => {
+  const Wrapper = Platform.OS === 'android' ? TouchableNativeFeedback : TouchableOpacity
+  return (
+    <Wrapper onPress={onPress} activeOpacity={0.8}>
+      <View style={{...styles.button, backgroundColor: color}}>
+        <AppTextBold style={styles.text}>{children}</AppTextBold>
+      </View>
+    </Wrapper>
+  )
+}
 
 const styles = StyleSheet.create({
   button: {
