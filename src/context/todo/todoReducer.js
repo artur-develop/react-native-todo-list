45a2103d9ea5cf7f5992e79bcb5dd5d395891b74
ayurkevich <artur.yurkevich.it@gmail.com>
2020-05-19 21:46:59 -0400

    Refactoring: Reducer

--- src/context/todo/todoReducer.js
+++ src/context/todo/todoReducer.js
@@ -1,28 +1,29 @@
 import {ADD_TODO, REMOVE_TODO, UPDATE_TODO} from '../types';
 
-export const todoReducer = (state, action) => {
-  switch (action.type) {
-    case ADD_TODO:
-      return {...state, todos: [...state.todos, {
-        id: Date.now().toString(),
-          title: action.title
-      }]}
-    case REMOVE_TODO:
-      return {
-        ...state,
-        todos: state.todo.filter(todo => todo.id !== action.id)
+const handlers = {
+  [ADD_TODO]: (state, {title}) => ({
+    ...state,
+    todos: [...state.todos, {
+      id: Date.now().toString(),
+      title
+    }]
+  }),
+  [REMOVE_TODO]: (state, {id}) => ({
+    ...state,
+    todos: state.todo.filter(todo => todo.id !== id)
+  }),
+  [UPDATE_TODO]: (state, {title, id}) => ({
+    ...state, todos: state.todos.map(todo => {
+      if (todo.id === id) {
+        todo.title = title
       }
-    case UPDATE_TODO:
-      return {...state, todos: state.todos.map(todo => {
-        if (todo.id === action.id) {
-          todo.title = action.title
-        }
-        return todo
-        })
-      }
-    default:
-      return state
-  }
+      return todo
+    })
+  }),
+  DEFAULT: state => state
+}
 
-  return state;
+export const todoReducer = (state, action) => {
+  const handler = handlers[action.type] || handlers.DEFAULT
+  return handler(state, action)
 }
